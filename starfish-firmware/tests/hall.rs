da1b1c4eaceaec8841a5d5adde13069301778a81
rishavchakra <rishavchakra@gmail.com>
2024-02-04 14:18:14 -0500

    Fixing/documenting Hall sensing

--- starfish-firmware/tests/hall.rs
+++ starfish-firmware/tests/hall.rs
@@ -1,12 +1,16 @@
 //! This tests the hall effect sensor by turning on
 //! the LED when a magnet is detected
 //! The Hall Effect Sensor should be plugged into GP1, GND, and GP2
+//!
+//! When using a single magnet, the sensor gets stuck on a high input
+//! Need to introduce an opposite magnetic field to "unstick" it and
+//! turn off the LED. This can be taken care of in the actual key switch mechanism
 #![no_std]
 #![no_main]
 
 use rp_pico as bsp;
 
-use bsp::entry;
+use bsp::{entry, hal::Clock};
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
@@ -20,12 +24,12 @@ use embedded_hal::digital::v2::PinState;
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
-    let _core = pac::CorePeripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
     let external_xtal_freq_hz = 12_000_000u32;
-    let _clocks = init_clocks_and_plls(
+    let clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
         pac.XOSC,
         pac.CLOCKS,
@@ -45,14 +49,23 @@ fn main() -> ! {
     );
 
     let mut pin_led = pins.led.into_push_pull_output();
-    let mut _vcc_pin = pins.gpio1.into_push_pull_output_in_state(PinState::High);
+    let mut pin_vcc = pins.gpio1.into_push_pull_output_in_state(PinState::High);
     let pin_hall_in = pins.gpio2.into_pull_up_input();
 
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
     loop {
+        // Power cycling, imitating the power intervals of a key matrix
+        pin_vcc.set_high().unwrap();
+        delay.delay_ms(100);
+
         if pin_hall_in.is_high().unwrap() {
             pin_led.set_high().unwrap();
         } else {
             pin_led.set_low().unwrap();
         }
+
+        pin_vcc.set_low().unwrap();
+        delay.delay_ms(300);
     }
 }
